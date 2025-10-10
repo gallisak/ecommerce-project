6f9e802c066c8d7378fb25c814f306ef1bf2377a
Andrii Halchyshak <agalchisak@gmail.com>
2025-10-10 21:26:54 +0300

    fix

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -3,12 +3,12 @@ import "./Page404.css";
 
 export function Page404() {
   return (
-    <>
+    <div className="div1">
       <Header />
 
       <title>Page 404</title>
 
       <h1>Page not found</h1>
-    </>
+    </div>
   );
 }
