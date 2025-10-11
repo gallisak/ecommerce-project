fe96874244b8bb5fbdda4ba18b701467389a0d71
Andrii Halchyshak <agalchisak@gmail.com>
2025-10-12 00:07:51 +0300

    Use axios to fetch the data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,12 @@
+import axios from "axios";
 import "./HomePage.css";
 import { products } from "../../starting-code/data/products";
 import { Header } from "../components/Header";
 
 export function HomePage() {
-  fetch("http://localhost:3000/api/products")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    });
+  axios.get("http://localhost:3000/api/products").then((response) => {
+    console.log(response.data);
+  });
 
   return (
     <>
